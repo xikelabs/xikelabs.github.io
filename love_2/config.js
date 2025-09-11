3930acc580b341adeecb6bf4805dc006d99774db
Nguyen Van Tuyen <tuyenwork1009@gmail.com>
2025-09-11 10:14:52 +0700

    .

--- love_2/config.js
+++ love_2/config.js
@@ -2,7 +2,7 @@
 const nameGirl = 'Em Yêu';
 const giftUrl = 'https://m.me/tuyen.fb/';
 const date = new Date();
-var day = date.getDay();
+var day = date.getDate();
 var month = date.getMonth() + 1;
 var eventName = 'Chúc Mừng '+day+'-'+month;
 const titleCard = 'Tặng Bé Yêu';
@@ -12,6 +12,6 @@ var images = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png'
     , '18.png', '18.png', '20.png', '21.png', '22.png', '23.png',
 ];
 var randomIndex = Math.floor(Math.random() * images.length);
-const giftImage = '../Love/images/'+ images[randomIndex];
+const giftImage = '../love_1/images/'+ images[randomIndex];
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
